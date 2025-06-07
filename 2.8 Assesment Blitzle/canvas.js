6c86d0a55e5a2c382409948bf9fb2055bc5bee7a
Makam12342 <22madams@wakatipu.school.nz>
2025-06-07 23:23:45 +1200

    Revert "function half added"
    This reverts commit 365491103497987b6314be26e46d6b73b9dfd14a.

--- 2.8 Assesment Blitzle/canvas.js
+++ 2.8 Assesment Blitzle/canvas.js
@@ -1,59 +1,27 @@
-function iskingUnderAtack(kingsSquare, oponentsPeices, oponenetsColor) {
-
-    //loops through all of the pieces
-    for(let pieceType in oponentsPeices){
-        const bitboard = oponentsPeices[pieceType] 
-        const pieceIndicies = hexToSquares(bitboard) // converts the bitboard to a list of indasies that that piece holds 
-    }
-
-    for(let peice in pieceIndicies){ // loops through each piece 
-        let moves = [] // a blank array ready for when it getts filled up with the diferent moves
-
-        // Generate moves based on piece type
-            if (pieceType.includes("Pawn")) {
-                moves = getPawnAttacks(peice, opponentColor);
-            } else if (pieceType.includes("Knight")) {
-                moves = validMovesStepper(peice, movedirections.stepPieces.knightMovements);
-            } else if (pieceType.includes("Bishop")) {
-                moves = validMovesSlider(peice, movedirections.sliderPieces.bishopMovements, opponentColor);
-            } else if (pieceType.includes("Rook")) {
-                moves = validMovesSlider(peice, movedirections.sliderPieces.rookMovements, opponentColor);
-            } else if (pieceType.includes("Queen")) {
-                moves = validMovesSlider(peice, movedirections.sliderPieces.queenMovements, opponentColor);
-            } else if (pieceType.includes("King")) {
-                moves = validMovesStepper(peice, movedirections.stepPieces.kingMovements);
+function sliderPieces(location, movelist) {
+    let validMoves = []
+    for(let i = 0; i < movelist.length; i++){ // gose through all of the diferent moves 
+        for(let j = 0; j < 7; j++){ //loops through the maxamium distance a [piece can move depending on the square
+            if(location + j*movelist[i] < 64){
+                validMoves.push(location + j*movelist[i])
             }
-
+        }
     }
-
 }
 
 
-function getPawnMoves(moves, row, col, pointerSquare, x) {
-    // foroward moves
-    // x is 1 or -1 depending on direction positive for black negative for white 
-    if(row === 1 & ((bitboard >> BigInt(pointerSquare - 16*x)) & 1n) === 0n & ((allWhitePiecesbitboard >> BigInt(pointerSquare -8)) & 1n) === 0n){
-    moves.push(-16*x)
-    }
-    if(((bitboard >> BigInt(pointerSquare -8*x)) & 1n) === 0n){
-    moves.push(-8*x)
-    }
-    // makes sure the piece is not edge hopping
-    if(((bitboard >> BigInt(pointerSquare - 9*x)) & 1n) !== 0n& col - 1 > 0 ){
-    moves.push(-9*x)
-    }
-    if(((bitboard >> BigInt(pointerSquare - 7*x)) & 1n) !== 0n & col + 1 < 8){
-    moves.push(-7*x)
+
+ function validMoves(pieceLocation, moveList) {
+        return moveList
+            for(let i = 0; i < 7; i++){
+                .map(offset => pieceLocation + offset*i)
+                .filter(target => target >= 0 && target < 64);
+                //.filter(target => target )
+            }
     }
 
-    // en pusant
-    if(((bitboard >> BigInt(pointerSquare + 1*x)) & 1n) !== 0n & col + 1 < 8){
-    moves.push(-7*x)
-    }
-    if(((bitboard >> BigInt(pointerSquare - 1*x)) & 1n) !== 0n & col - 1 > 0){
-    moves.push(-9*x)
-    }
-    return moves
+
+    function validMovesSlider(pieceLoacation, movelist)
+{
+    
 }
-
-getPawnMoves(moves, row, col, 1)
