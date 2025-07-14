08bdf40f6492cdb84baa01e3fd4f07842bbcdebb
Makam12342 <22madams@wakatipu.school.nz>
2025-07-14 20:36:12 +1200

    tasks added to do before end of sprint

--- public/canvas.js
+++ public/canvas.js
@@ -1,3 +1,18 @@
-/// tasks:sfix move history so it shows both players moves, add iccons
-/// clean up:make video, post website, write up feedback
+
 // win lose and draw screen tidy up have the tags reflect real info n player count
+
+// do all tasks before end of sprint
+//Add board flipping
+//Add player count to tags
+//Add info to the info page
+//Make the timer a global asset so the time is the same on each side 
+//Disable game when finished 
+//Add some better indication on whose turn it is
+//Improve end screens 
+// fix mouse flickering 
+// add timer options
+//Better icons
+//Show removed pieces
+//Add chat
+//Remove rooms after 30 mins
+//Highlight square where piece moved to
