5349876aacea0f5dc8706f84a4d208b683bf97dc
Makam12342 <22madams@wakatipu.school.nz>
2025-07-14 22:35:03 +1200

    Info page filled out with info

--- public/canvas.js
+++ public/canvas.js
@@ -2,17 +2,32 @@
 // win lose and draw screen tidy up have the tags reflect real info n player count
 
 // do all tasks before end of sprint
+    
 //Add board flipping
+    // flip the board, mouse inut, and icons sepritly 
 //Add player count to tags
+    // Task 2
 //Add info to the info page
-//Make the timer a global asset so the time is the same on each side 
+    //Task 1
+//Make the timer a global asset so the time is the same on each side
+    //Task 4
 //Disable game when finished 
+    // Tast 3
 //Add some better indication on whose turn it is
+    // Task 5
 //Improve end screens 
+    // Task 6
 // fix mouse flickering 
+    // Task 7
 // add timer options
+    // Task 8
 //Better icons
+    // Task 9
 //Show removed pieces
+    // Task 10
 //Add chat
+    // task 13
 //Remove rooms after 30 mins
+    // task 11
 //Highlight square where piece moved to
+    // task 12
